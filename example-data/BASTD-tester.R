cdfa346896ce18db7045e1bf7dd3db58900ac1ca
teamOSTAP <69810006+teamOSTAP@users.noreply.github.com>
2021-04-14 09:02:38 +0100

    Updated BASTD tester

--- example-data/BASTD-tester.R
+++ example-data/BASTD-tester.R
@@ -15,26 +15,19 @@ load.packages(packages)
 
 #To install the retimes package, required for BASTD, you will need to install retimes from the CRAN archive
 #retimes will require you to have Xcode install (see: https://stackoverflow.com/questions/24194409/how-do-i-install-a-package-that-has-been-archived-from-cran)
-install_url("https://cran.r-project.org/src/contrib/Archive/retimes/retimes_0.1-2.tar.gz") #this will install xcode if you do not already have it installed, followed by retimes
-library(retimes) #initialise retimes
+# install_url("https://cran.r-project.org/src/contrib/Archive/retimes/retimes_0.1-2.tar.gz") #this will install xcode if you do not already have it installed, followed by retimes
+# library(retimes) #initialise retimes
 
 # BASTD tester ------------------------------------------------------------
 # install the latest version of the package -------------------------------
-install_github("HeJasonL/BASTD", force = TRUE) #install latest version of BASTD from GitHub
+install_github("teamOSTAP/BASTD", force = TRUE) #install latest version of BASTD from GitHub
 library(BASTD) #read the package into the library
 
-# STOP-IT -----------------------------------------------------------------
-example_STOP_IT_data <- "https://raw.githubusercontent.com/HeJasonL/BASTD/master/example-data/STOP-IT_raw.csv"
-STOP_IT_data <- read.csv(example_STOP_IT_data, header = TRUE) #read the example STOP-IT data
-BASTD_analyze(data = STOP_IT_data, maximum_go_trial_RT = 1200) #BASTD_analyze
-
 # OSARI  ------------------------------------------------------------------
-example_OSARI_data <- "https://raw.githubusercontent.com/HeJasonL/BASTD/master/example-data/OSARI_raw.txt"
+example_OSARI_data <- "https://raw.githubusercontent.com/teamOSTAP/BASTD/main/example-data/OSARI_raw.txt"
 OSARI_data <- read.csv(example_OSARI_data, header = TRUE, sep = "") #read the example OSARI data
-OSARI_analyze(data = OSARI_data) #OSARI analyze
-
+OSARI_analyze(data = OSARI_data) #OSARI analyze - produces all the values including SSRT
 
 # OSARI visualized --------------------------------------------------------
 OSARI_visualize(data = OSARI_data)
-
 
